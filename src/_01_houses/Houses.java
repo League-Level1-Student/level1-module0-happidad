ca75488de149eb17a62f2c1460c5017bc5aee555
Kevin Lee <kevindlee@gmail.com>
2019-11-04 07:02:00 -0800

    Use string to specify house size.

--- src/_01_houses/Houses.java
+++ src/_01_houses/Houses.java
@@ -3,43 +3,55 @@ package _01_houses;
 import org.jointheleague.graphical.robot.Robot;
 
 public class Houses {
-	private Robot rob;
-	Houses(){
-		this.rob = new Robot();
-	}
+    private Robot rob;
 
-	public void drawHouse(int height) {
-		rob.setPenColor(255,0,0);
-		rob.setSpeed(100);
-		rob.penDown();
-		// draw left side
-		rob.move(height);
-		// draw roof
-		rob.turn(90);
-		rob.move(30);
-		rob.turn(90);
-		// draw right side
-		rob.move(height);
-		// draw green grass
-		rob.turn(-90);
+    Houses() {
+        this.rob = new Robot();
+    }
 
-		rob.setPenColor(96,128,56);
-		rob.move(30);
-		rob.turn(-90);
-	}
-	public void run() {
-		// move to initial position, lower left corner
-		rob.moveTo(10,300);
+    public void drawHouse(String size) {
+        if (size.equals("small")) drawHouse(60);
+        else if (size.equals("medium")) drawHouse(120);
+        else if (size.equals("large")) drawHouse(250);
+    }
 
-		for (int i = 0; i <10 ; i++) {
-			drawHouse((i+1)*10);
-		}
+    public void drawHouse(int height) {
+        rob.setPenColor(255, 0, 0);
+        rob.setSpeed(100);
+        rob.penDown();
+        // draw left side
+        rob.move(height);
+        // draw roof
+        rob.turn(90);
+        rob.move(30);
+        rob.turn(90);
+        // draw right side
+        rob.move(height);
+        // draw green grass
+        rob.turn(-90);
+
+        rob.setPenColor(96, 128, 56);
+        rob.move(30);
+        rob.turn(-90);
+    }
+
+    public void run() {
+        // move to initial position, lower left corner
+        rob.moveTo(10, 500);
+
+        for (int i = 0; i < 10; i++) {
+            drawHouse((i + 1) * 10);
+        }
+
+        drawHouse("small");
+        drawHouse("medium");
+        drawHouse("large");
 
 
-		// move robot out of the way
-		rob.penUp();
-		rob.move(500);
+        // move robot out of the way
+        rob.penUp();
+        rob.move(500);
 
-		
-	}
+
+    }
 }
